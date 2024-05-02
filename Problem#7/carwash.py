14329867208da2b490eed78a31772d19a042e399
madpoetwithapen <christinekparrish2023@gmail.com>
2024-05-02 17:08:07 -0500

    We Cool?

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -1,31 +1,31 @@
 def calculate_car_wash_price(service_choice1, service_choice2):
-    services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
-    base_wash = 10
-    total = int(base_wash)
-    value = services.get(service_choice1)
-    total += int(value)
-    value_2 = services.get(service_choice2)
-    total += int(value_2)
+        services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
+        base_wash = 10
+        total = int(base_wash)
+        value = services.get(service_choice1)
+        total += int(value)
+        value_2 = services.get(service_choice2)
+        total += int(value_2)
     
-    print("ZyCar Wash")
+        print("ZyCar Wash")
 
-    print("Base car wash - $10")
+        print("Base car wash - $10")
 
     
 
-    if service_choice1 != "-":
+        if service_choice1 != "-":
 
-        print(service_choice1 + " - $" + str(services.get(service_choice1)))
+            print(service_choice1 + " - $" + str(services.get(service_choice1)))
 
-    if service_choice2 != "-":
+        if service_choice2 != "-":
 
-        print(service_choice2 + " - $" + str(services.get(service_choice2)))
+            print(service_choice2 + " - $" + str(services.get(service_choice2)))
 
-    print("-----")
+        print("-----")
     
-    print("Total price: $" + str(total)) 
+        print("Total price: $" + str(total)) 
     
-    return
+        return
 
 
 
